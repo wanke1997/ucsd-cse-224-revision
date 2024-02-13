d78a27d4cd3447da27abf0ea202d2a5ee137a5de
wanke1997 <ke_wan_2019@hotmail.com>
2024-02-13 00:21:56 -0800

    fixed variable names

--- PA2/src/netsort.go
+++ PA2/src/netsort.go
@@ -37,7 +37,7 @@ func readServerConfigs(configPath string) ServerConfigs {
 }
 
 // golang server
-func listen(client_server_wg *sync.WaitGroup, Host string, Port string, amount int) {
+func listen(clientServerWG *sync.WaitGroup, Host string, Port string, amount int) {
 	address := Host + ":" + Port
 	ln, err := net.Listen("tcp", address)
 	if err != nil {
@@ -58,7 +58,7 @@ func listen(client_server_wg *sync.WaitGroup, Host string, Port string, amount i
 		go handleConnection(conn, i, &wg)
 	}
 	wg.Wait()
-	client_server_wg.Done()
+	clientServerWG.Done()
 }
 
 // thread handler to deal with detailed work
@@ -66,14 +66,14 @@ func handleConnection(conn net.Conn, conn_id int, wg *sync.WaitGroup) {
 	bs := make([]byte, 4)
 	conn.Read(bs)
 	str := string(bs)
-	text := "This is " + str + " connection.\n"
+	text := "This is connection with server " + str + ".\n"
 	print(text)
 	conn.Close()
 	wg.Done()
 }
 
 // golang client
-func dial(client_server_wg *sync.WaitGroup, scs ServerConfigs, serverId int) {
+func dial(clientServerWG *sync.WaitGroup, scs ServerConfigs, serverId int) {
 	for i := 0; i < len(scs.Servers); i++ {
 		if i == serverId {
 			continue
@@ -87,7 +87,7 @@ func dial(client_server_wg *sync.WaitGroup, scs ServerConfigs, serverId int) {
 		bs := []byte(strconv.Itoa(serverId))
 		conn.Write(bs)
 	}
-	client_server_wg.Done()
+	clientServerWG.Done()
 }
 
 func main() {
@@ -110,10 +110,10 @@ func main() {
 	fmt.Println("my host name:", scs.Servers[serverId].Host)
 
 	// create a client, a server, and a mutex
-	var client_server_wg sync.WaitGroup
-	client_server_wg.Add(2)
-	go listen(&client_server_wg, scs.Servers[serverId].Host, scs.Servers[serverId].Port, amount)
+	var clientServerWG sync.WaitGroup
+	clientServerWG.Add(2)
+	go listen(&clientServerWG, scs.Servers[serverId].Host, scs.Servers[serverId].Port, amount)
 	time.Sleep(3 * time.Second)
-	go dial(&client_server_wg, scs, serverId)
-	client_server_wg.Wait()
+	go dial(&clientServerWG, scs, serverId)
+	clientServerWG.Wait()
 }
